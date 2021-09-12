cb9d7afb2c003ecf596a46ae4b6c8a33139ec0b4
luckyhyom <bs_khm@naver.com>
2021-09-12 23:36:24 +0900

    Entity '객체'로써 활용하기.(createOne 메소드 생성), repository는 쿼리에 관한 코드(DTO)

--- src/lecture/lecture.entity.ts
+++ src/lecture/lecture.entity.ts
@@ -29,4 +29,30 @@ export class Lecture extends BaseEntity {
 
     @ManyToOne(type => User, user => user.lecture)
     user: User
+
+    static createOne(title:string, user: User) {
+        const lecture = new Lecture();
+        lecture.title = title;
+        lecture.description = '테스트 객체';
+        lecture.videoURL = 'www.test.co.kr';
+        lecture.author = '테스트';
+        lecture.price = 100;
+        lecture.viewStatus = true;
+        lecture.userId = user.id;
+        lecture.user = user;
+
+        return lecture;
+    }
+
+    /**
+     * 객체마다 자신이 가진 옵션이 다르다.
+     * 지금 이 객체는 viewState라는 옵션 하나만 가지고 있지만
+     * 나중에 다른 객체는 '공개'를 하기 위해 설정을 바꿀 부분이 많을 수도 있다.
+     * 혹은 설정이 하나더라도 옵션 이름이 다를 수도 있다.
+     * 하지만 결국 원하는 것은 '공개하기'이니까, Service는 그저 '공개 해주셈' 하면 된다.
+     * => service에서 주입받는 entity는 공통적으로 publish라는 함수를 구현해야함.
+     */
+    publish() {
+        this.viewStatus = true;
+    }
 }
