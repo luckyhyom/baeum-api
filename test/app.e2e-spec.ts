4f1692c264c2a78184f9480781984f04c8c3ec95
luckyhyom <bs_khm@naver.com>
2021-09-16 10:58:26 +0900

    간단한 e2e테스트. toMatchObject

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from 'src/app.module';
+import { Lecture } from 'src/lecture/lecture.entity';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +16,11 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('Get all [GET /lecture]', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .get('/lecture')
+      .then(lectures => {
+        expect(lectures).toMatchObject(Promise.resolve([Lecture]))
+      })
   });
 });
