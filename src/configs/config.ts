a228f0c7ed2192161069c79e02239494c9c8ce2a
luckyhyom <bs_khm@naver.com>
2021-10-26 03:41:49 +0900

    ENV파일 없이 sqlite 인메모리로 테스트 가능하게 설정

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -12,20 +12,20 @@ function required(key, defaultValue = undefined) {
 export const config = {
     port: required('HOST_PORT',5432),
     db: {
-        host: required('DB_HOST'),
-        username: required('DB_USER'),
-        database: required('DB_DATABASE'),
-        password: required('DB_PASSWORD'),
+        host: required('DB_HOST','localhost'),
+        username: required('DB_USER', 'inmemory'),
+        database: required('DB_DATABASE', 'inmemory'),
+        password: required('DB_PASSWORD', 'inmemory'),
     },
     csrf: {
-        password: required('CSRF_PASSWORD')
+        password: required('CSRF_PASSWORD','test')
     },
     aws: {
         s3: {
-            name: required('AWS_S3_BUCKET_NAME'),
-            region: required('AWS_S3_REGION'),
-            access_key: required('AWS_S3_ACCESS_KEY'),
-            secret_key: required('AWS_S3_SECRET_KEY'),
+            name: required('AWS_S3_BUCKET_NAME', 'inmemory'),
+            region: required('AWS_S3_REGION', 'inmemory'),
+            access_key: required('AWS_S3_ACCESS_KEY', 'inmemory'),
+            secret_key: required('AWS_S3_SECRET_KEY', 'inmemory'),
         }
     }
 }
